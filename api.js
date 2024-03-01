f911fc66b0f29b532ee66519a2ecb9da56512583
Benjamin Belizaire <BenjaminBelizaire@gmail.com>
2024-03-01 14:49:10 -0500

    modified api

--- api.js
+++ api.js
@@ -4,34 +4,32 @@ require("mongodb");
 
 exports.setApp = function (app, client) {
 
-
     //sign up endpoint logic
     app.post('/api/signup', async (req, res) => {
         // incoming fN, lN, email, login, password
-        const { firstName, lastName, email, login, password } = req.body;
-        const newUser = {FirstName: firstName, LastName: lastName, Email: email, Password: password };
+        const { firstName, lastName, email, password } = req.body;
+        const newUser = { FirstName: firstName, LastName: lastName, Email: email, Password: password };
         try {
             const db = client.db();
             const result = await db.collection('Users').insertOne(newUser);
-            const insertedId = result.insertedId
-            const UserID = insertedId; // Assign _id to UserID
-            await db.collection('Users').updateOne({ _id: insertedId }, { $set: { UserID: UserID } }); // Set UserID to the _id
-            res.status(200).json({ id: UserID, error: '' });
+            const insertedId = result.insertedId;
+            res.status(200).json({ id: insertedId.toString(), error: '' });
         } catch (e) {
             res.status(500).json({ error: e.toString() });
         }
     });
+
     // login endpoint logic
     app.post('/api/login', async (req, res) => {
         const { email, password } = req.body;
         try {
             const db = client.db();
-            console.log('Attempting to find user with login:', email); // trouble shooting
-            const user = await db.collection('Users').findOne({ Email:email, Password: password });
+            console.log('Attempting to find user with email:', email); // trouble shooting
+            const user = await db.collection('Users').findOne({ Email: email, Password: password });
             console.log('User found:', user); // trouble shooting
             if (user) {
-                const { _id, FirstName, LastName } = user;
-                res.status(200).json({ id: _id, firstName: FirstName, lastName: LastName, error: '' });
+                const { UserID, FirstName, LastName } = user;
+                res.status(200).json({ id: UserID, firstName: FirstName, lastName: LastName, error: '' });
             } else {
                 res.status(401).json({ error: 'Invalid login credentials' });
             }
@@ -41,5 +39,3 @@ exports.setApp = function (app, client) {
     });
 
 }
-
-
