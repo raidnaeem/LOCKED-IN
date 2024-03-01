e44aa0eed1b8da96f7f99916eb21d38b513a0c3f
Andy Van <oneandyvan@gmail.com>
2024-03-01 04:26:50 -0500

    use env var in test.js (I don't know if we use this)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://locked-in:Prayers1Fire@locked-in.iacd1hk.mongodb.net/?retryWrites=true&w=majority&appName=locked-in";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
