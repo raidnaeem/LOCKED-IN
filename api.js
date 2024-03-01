6a89c54f4737fcbbb446cf3ba3b4a2f3992bf6d4
Benjamin Belizaire <BenjaminBelizaire@gmail.com>
2024-03-01 15:43:30 -0500

    testing api

--- api.js
+++ api.js
@@ -4,28 +4,14 @@ require("mongodb");
 
 exports.setApp = function (app, client) {
      // Function to fetch the next sequence value for UserID
-     const getNextSequenceValue = async (db, sequenceName) => {
-        const result = await db.collection("UserCounter").findOneAndUpdate(
-            { _id: sequenceName },
+     const getNextSequenceValue = async (db) => {
+        const result = await db.collection("Users").findOneAndUpdate(
+            { _id: "userId" },
             { $inc: { sequence_value: 1 } },
-            { returnOriginal: false }
+            { upsert: true, returnOriginal: false }
         );
         return result.value.sequence_value;
     };
-
-    // Create UserCounter collection and insert initial sequence value
-    const initializeUserCounter = async () => {
-        try {
-            const db = client.db();
-            await db.createCollection("UserCounter");
-            await db.collection("UserCounter").insertOne({ _id: "userId", sequence_value: 0 });
-        } catch (e) {
-            console.error("Error initializing UserCounter collection:", e);
-        }
-    };
-
-    initializeUserCounter(); // Initialize UserCounter collection
-
 
     //sign up endpoint logic
     app.post('/api/signup', async (req, res) => {
@@ -33,7 +19,7 @@ exports.setApp = function (app, client) {
         const { firstName, lastName, email, password } = req.body;
         try {
             const db = client.db();
-            const userID = await getNextSequenceValue(db, 'userId');
+            const userID = await getNextSequenceValue(db);
             const newUser = { FirstName: firstName, LastName: lastName, Email: email, Password: password };
             await db.collection('Users').insertOne(newUser);
             res.status(200).json({ id: insertedId.toString(), error: '' });
