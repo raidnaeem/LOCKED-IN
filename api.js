2adfca9130c28aff573d09f14f7e26787ddc3d40
Benjamin Belizaire <BenjaminBelizaire@gmail.com>
2024-03-01 12:07:44 -0500

    should work for login now

--- api.js
+++ api.js
@@ -28,7 +28,8 @@ exports.setApp = function (app, client) {
             const user = await db.collection('Users').findOne({ Login: login, Password: password });
             console.log('User found:', user); // trouble shooting
             if (user) {
-                const { UserID, FirstName, LastName } = user;
+                const { _id, FirstName, LastName } = user;
+                const UserID = _id;
                 res.status(200).json({ id: UserID, firstName: FirstName, lastName: LastName, error: '' });
             } else {
                 res.status(401).json({ error: 'Invalid login credentials' });
