9eae1d4c7a1fd2a5397caa9c8ace8baf835cee80
Andy Van <oneandyvan@gmail.com>
2024-03-05 16:09:24 -0500

    Copied frontend stuff from MERN A/B lab

---
+++ frontend/src/components/PageTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PageTitle()
+{
+   return(
+     <h1 id="title">Locked In</h1>
+   );
+};
+
+export default PageTitle;
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const path = require('path');           
+const PORT = process.env.PORT || 5000;  
 const app = express();
+app.set('port', (process.env.PORT || 5000));
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -28,10 +32,24 @@ app.use((req, res, next) =>
     next();
 });
 
+// Server static assets if in production (Heroku deployment)
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 // just adding in for api 
 var api = require('./api.js');
 api.setApp(app, client);
 
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
 
-app.listen(5000); // start Node + Express server on port 5000
