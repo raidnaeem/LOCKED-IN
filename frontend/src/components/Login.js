3b0cde4ada568a09ad4b5b4403553e4070c7b281
AndrewZ496 <an719055@ucf.edu>
2024-03-06 14:43:00 -0500

    Updated Login UI

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,64 +1,79 @@
 import React, { useState } from 'react';
 import { useJwt } from "react-jwt";
+import LoginUI from './LoginUI'; // Import the LoginUI component
 var bp = require('./Path.js');
 
-
-function Login()
-{
+function Login() {
     var loginEmail;
     var loginPassword;
 
-    const [loginMessage,setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-    const doLogin = async event => 
-    {
+    const doLogin = async (event) => {
         event.preventDefault();
 
-        var obj = {Email:loginEmail.value,Password:loginPassword.value};
+        var obj = { Email: loginEmail.value, Password: loginPassword.value };
         var js = JSON.stringify(obj);
 
-        try
-        {    
-            const response = await fetch(bp.buildPath('api/login'),
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+        try {
+            const response = await fetch(bp.buildPath('api/login'), {
+                method: 'POST',
+                body: js,
+                headers: { 'Content-Type': 'application/json' },
+            });
 
             var res = JSON.parse(await response.text());
 
-            if( res.status == 200)
-            {
-                var user = {FirstName:res.FirstName,LastName:res.LastName,UserID:res.UserID}
+            if (res.id === 400) {
+                setMessage('User/Password combination incorrect');
+            } else if (res.id === 200) {
+                var user = { firstName: res.firstName, lastName: res.lastName, id: res.id };
                 localStorage.setItem('user_data', JSON.stringify(user));
 
                 setMessage('');
-                window.location.href = '/planner';
+                window.location.href = '/cards';
+            } else {
+                setMessage(res);
             }
-            else
-            {
-                setMessage(res.error);
-            }
-        }
-        catch(e)
-        {
+        } catch (e) {
             alert(e.toString());
             return;
-        }    
+        }
     };
 
+    return (
+        <div id="loginDiv">
+            {/* Use the LoginUI component here */}
+            <LoginUI />
 
-    return(
-      <div id="loginDiv">
-        <form onSubmit={doLogin}>
-        <span id="inner-title">PLEASE LOG IN</span><br />
-        <input type="text" id="loginEmail" placeholder="Email" 
-          ref={(c) => loginEmail = c} /><br />
-        <input type="password" id="loginPassword" placeholder="Password" 
-          ref={(c) => loginPassword = c} /><br />
-        <input type="submit" id="loginButton" class="buttons" value = "Do It"
-          onClick={doLogin} />
-        </form>
-        <span id="loginResult">{loginMessage}</span>
-     </div>
+            <form onSubmit={doLogin}>
+                <span id="inner-title">PLEASE LOG IN</span>
+                <br />
+                <input
+                    type="text"
+                    id="loginEmail"
+                    placeholder="Email"
+                    ref={(c) => (loginEmail = c)}
+                />
+                <br />
+                <input
+                    type="password"
+                    id="loginPassword"
+                    placeholder="Password"
+                    ref={(c) => (loginPassword = c)}
+                />
+                <br />
+                <input
+                    type="submit"
+                    id="loginButton"
+                    className="buttons"
+                    value="Do It"
+                    onClick={doLogin}
+                />
+            </form>
+            <span id="loginResult">{message}</span>
+        </div>
     );
-};
+}
 
 export default Login;
