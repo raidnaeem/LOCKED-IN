6fd672367010f6acc501d44da42ed8bb4e557ab2
AndrewZ496 <an719055@ucf.edu>
2024-03-06 15:19:11 -0500

    Update LoginUI

--- frontend/src/components/LoginUI.jsx
+++ frontend/src/components/LoginUI.jsx
@@ -8,7 +8,7 @@ const LoginUI = () => {
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <div style={{ maxWidth: '600px', width: '100%' }}>
+            <div style={{ maxWidth: '600px', width: '80%' }}>
                 <div style={{ width: 584, height: 247, left: 0, top: 99, position: 'absolute', background: '#EDF2F7' }} />
                 <div style={{ width: 490.61, height: 542.47, left: 78.50, top: 130.53, position: 'absolute', background: '#667EEA', boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.12)' }} />
                 <div style={{ width: 490.61, height: 542.47, left: 67.60, top: 119.83, position: 'absolute', background: 'white', boxShadow: '6px 0px 12px rgba(0, 0, 0, 0.12)' }} />
