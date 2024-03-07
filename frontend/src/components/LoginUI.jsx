4aea72e60c336e8dfca6b7ff4d4249fd8258b70f
AndrewZ496 <an719055@ucf.edu>
2024-03-07 16:06:40 -0500

    Update LoginUI

--- frontend/src/components/LoginUI.jsx
+++ frontend/src/components/LoginUI.jsx
@@ -64,8 +64,8 @@ const LoginUI = ({ doLogin, loginEmail, setLoginEmail, loginPassword, setLoginPa
                     </div>
                 </div>
 
-                <div style={{ width: 439.37, height: 53.50, left: 90.49, top: 576.70, position: 'absolute', background: 'white', border: '1px #4A5568 solid' }} />
-                <div style={{ ...commonTextStyle, fontSize: 18, color: '#4A5568', fontWeight: '500', width: 195.15, height: 22.47, left: 213.69, top: 592.75, position: 'absolute' }}>Register New Account</div>
+                <div style={{ width: 439.37, height: 53.50, left: 90.49, top: 585.70, position: 'absolute', background: 'white', border: '1px #4A5568 solid' }} />
+                <div style={{ ...commonTextStyle, fontSize: 18, color: '#4A5568', fontWeight: '500', width: 195.15, height: 22.47, left: 213.69, top: 600.75, position: 'absolute' }}>Register New Account</div>
 
                 <div style={{ width: 439.37, height: 1.07, left: 92.67, top: 370.20, position: 'absolute', background: 'rgba(102, 126, 234, 0.50)' }} />
                 <div style={{ left: 180, top: 539, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', width: '300px' }}>Forgot password? Click here to reset</div>
