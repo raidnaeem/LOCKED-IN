5f3f5847918f29866d730c2690b0a2efe5f9680c
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2024-03-28 19:11:27 -0400

    Update routes.dart

--- mobile/lib/routes/routes.dart
+++ mobile/lib/routes/routes.dart
@@ -1,10 +1,19 @@
+import 'package:flutter_app/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/login_screen.dart';
-class Routes {
-  static const String LOGINSCREEN = '/login';
-  // routes of pages in the app
-  static Map<String, Widget Function(BuildContext)> get getroutes => {
-  '/': (context) => LoginScreen(),
-    LOGINSCREEN: (context) => LoginScreen(),
-  };
+
+void main() {
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: '',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(),
+        routes: Routes.getroutes,
+      );
+    }
+  }
