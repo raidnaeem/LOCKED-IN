bf31e96a23fee0dd428bb0636baa9611a14f07a2
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2024-04-04 19:28:02 -0400

    Near completion on login and register
    Stylistic Updates

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,19 +1,217 @@
-import 'package:mobile/routes/routes.dart';
+import 'package:mobile/homepage.dart';
+import 'verification.dart';
 import 'package:flutter/material.dart';
+import 'token.dart';
+import 'register.dart';
 
-void main() {
-  runApp(MyApp());
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  createState() => _LoginPageState();
 }
 
-class MyApp extends StatelessWidget {
-// This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: '',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(),
-        routes: Routes.getroutes,
+String _email = '';
+String _password = '';
+
+class _LoginPageState extends State<LoginPage> {
+  void _handleEmailChange(String value) {
+    setState(() {
+      _email = value;
+    });
+
+  }
+
+  void _handlePasswordChange(String value) {
+    setState(() {
+      _password = value;
+    });
+  }
+
+  Future<void> _handleSubmit(context) async {
+    try {
+      await login(_email, _password);
+      print(decodedToken);
+
+      if (decodedToken['verified'] == true) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePageScreen()),
+        );
+
+      }
+
+    }
+    catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('$e'), // Convert the error to a string to display
+          duration: const Duration(seconds: 10), // Adjust the duration as needed
+        ),
       );
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'LockedIn',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 60,
+                      fontFamily: 'Arial Narrow',
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 350, // Set width of the container
+                        child: Container(
+                          color: Colors.grey[200],
+                          child: TextFormField(
+                            onChanged: _handleEmailChange,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              hintText: 'Example@gmail.com',
+                              hintStyle: const TextStyle(
+                                  color: Colors.black54, fontFamily: 'Arial Narrow'),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  12.0, 8.0, 12.0, 8.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Password',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 350, // Set width of the container
+                        child: Container(
+                          color: Colors.grey[200],
+                          child: TextFormField(
+                            onChanged: _handlePasswordChange,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your password',
+                              hintStyle: const TextStyle(
+                                  color: Colors.black54, fontFamily: 'Arial Narrow'),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  12.0, 8.0, 12.0, 8.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: TextButton(
+                      onPressed: () {
+                        // Implement your forgot password logic here
+                      },
+                      child: const Text(
+                        'Forgot password?',
+                        style: TextStyle(color: Colors.blue, fontSize: 17),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Builder(
+                    builder: (context) => MaterialButton(
+                      onPressed: () => _handleSubmit(context),
+                        color: const Color(0xFF14532d).withBlue(255),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 13.0),
+                      child: const SizedBox(
+                        width: 350,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.white, fontSize: 18, fontFamily: 'Arial Narrow'),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Register()),
+                      );
+                    },
+                    child: const Text(
+                      'Need an account? Sign up',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontFamily: 'Arial Narrow',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LockedIn',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ mobile/lib/token.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+dynamic token;
+late String userId;
+late var decodedToken = {};
+
+Future<void> signUp(
+    String firstName, String lastName, String email, String password) async {
+  final Uri url =
+  Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/users/signup');
+  final Map<String, String> body = {
+    'firstName': firstName,
+    'lastName': lastName,
+    'email': email,
+    'password': password,
+  };
+
+  try {
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 201) {
+      final jsonResponse = json.decode(response.body);
+      token = jsonResponse['token']; // Extracting the token string
+      decodedToken = JwtDecoder.decode(token);
+
+      //userId
+      userId = decodedToken['userId'];
+    } else {
+      // Signup failed
+      throw 'Signup failed';
+    }
+  } catch (e) {
+    // Exception occurred
+    throw 'Signup failed';
+  }
+}
+
+Future<void> login(String email, String password) async {
+  final Uri url = Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/login');
+  final Map<String, String> body = {
+    'Email': email,
+    'Password': password,
+  };
+
+  try {
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 201) {
+      final jsonResponse = json.decode(response.body);
+      token = jsonResponse['token']; // Extracting the token string
+      decodedToken = JwtDecoder.decode(token);
+
+      //userId
+      userId = decodedToken['userId'];
+
+      print(userId);
+      print(decodedToken['firstName']);
+
+    } else if(response.statusCode == 404) {
+      // Login failed
+      throw 'Invalid Email';
+    }
+    else
+    {
+      throw 'Invalid Password';
+    }
+  }
+
+  catch (e) {
+    // Exception occurred
+  }
+}
+
+Future<void> sendCode() async {
+  final Uri url = Uri.parse(
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/sendVerification');
+  final Map<String, String> body = {
+    'id': userId,
+  };
+
+  try {
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      var sent = jsonResponse['sent'];
+
+      if (sent == false) {
+        throw Exception('Failed to send verification code');
+      }
+    }
+  } catch (e) {
+    // Exception occurred
+    print('$e');
+    // You might want to handle this error in your UI
+  }
+}
+
+Future<void> verifyUser(String code) async {
+  final Uri url = Uri.parse(
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/verifyUser');
+  final Map<String, String> body = {
+    'id': userId,
+    'code': code,
+  };
+
+  try {
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode(body),
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      var verified = jsonResponse['verified'];
+
+      if (verified == false) {
+        throw 'Unable to Verify User';
+      }
+    }
+  }
+  catch (e) {
+    // Exception occurred
+  }
+}
+
+
+
