2423dc223af0cd991b2757aaea14d5e4a21284d4
AndrewZ496 <an719055@ucf.edu>
2024-04-04 18:23:03 -0400

    Making timer continue when switching pages

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -11,7 +11,6 @@ const Timer = () => {
   const [isEditable, setIsEditable] = useState(false);
   const [inputMinutes, setInputMinutes] = useState('');
   const [inputSeconds, setInputSeconds] = useState('');
-
   const audioRef = useRef(null);
 
   const playSound = () => {
@@ -20,39 +19,20 @@ const Timer = () => {
     }
   };
 
-  const handleMouseDown = (e) => {
-    if (e.target === timerRef.current) {
-      e.preventDefault();
-      setIsDragging(true);
-      const rect = timerRef.current.getBoundingClientRect();
-      setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-    }
-  };
-
+  // Save timer state to localStorage
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      if (isDragging) {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY,
-        });
-      }
-    };
+    localStorage.setItem('timerData', JSON.stringify({ time, isRunning, pausedAt }));
+  }, [time, isRunning, pausedAt]);
 
-    const handleMouseUp = () => {
-      setIsDragging(false);
-    };
-
-    if (isDragging) {
-      document.addEventListener('mousemove', handleMouseMove);
-      document.addEventListener('mouseup', handleMouseUp);
+  // Retrieve timer state from localStorage
+  useEffect(() => {
+    const timerData = JSON.parse(localStorage.getItem('timerData'));
+    if (timerData) {
+      setTime(timerData.time);
+      setIsRunning(timerData.isRunning);
+      setPausedAt(timerData.pausedAt);
     }
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, [isDragging]);
+  }, []);
 
   const handleStartStop = () => {
     if (isRunning) {
