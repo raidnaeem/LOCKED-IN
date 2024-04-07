3cae999c380c1f78891fab8dc758775eca1334f3
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2024-04-07 17:29:13 -0400

    Update verification.dart style

--- mobile/lib/verification.dart
+++ mobile/lib/verification.dart
@@ -14,10 +14,10 @@ class _VerificationPageState extends State<VerificationPage> {
   void initState() {
     super.initState();
     // Call sendVerification when the page is initialized
-
   }
 
   String _verificationCode = '';
+  String _email = ''; // Added email field
 
   void _handleVerificationCode(String value) {
     setState(() {
@@ -25,7 +25,13 @@ class _VerificationPageState extends State<VerificationPage> {
     });
   }
 
-  void _handleEmail(BuildContext context) async {
+  void _handleEmail(String value) {
+    setState(() {
+      _email = value;
+    });
+  }
+
+  void _handleSubmit(BuildContext context) async {
     try {
       await verifyUser(_verificationCode);
 
@@ -43,6 +49,20 @@ class _VerificationPageState extends State<VerificationPage> {
     }
   }
 
+  void _handleSendCode(BuildContext context) async {
+    final currentContext = context;
+    try {
+      //await sendCode(_email); // Pass email to sendCode function
+    } catch (e) {
+      if (!currentContext.mounted) return;
+      ScaffoldMessenger.of(currentContext).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred. Please try again...'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,14 +72,72 @@ class _VerificationPageState extends State<VerificationPage> {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width * 0.8,
+          width: MediaQuery.of(context).size.width * 0.8,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(height: 15), // Added SizedBox
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'LockedIn',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 60,
+                    fontFamily: 'Arial Narrow',
+                  ),
+                ),
+              ), // Added Align
+              const SizedBox(height: 30.0),
+              const Text(
+                'Enter Email:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              TextField(
+                onChanged: _handleEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  hintText: 'Example@email.com',
+                  hintStyle: const TextStyle(
+                    color: Colors.black54,
+                    fontFamily: 'Arial Narrow',
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 12.0),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () => _handleSubmit(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF14532d).withBlue(255),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 13.0),
+                ),
+                child: const SizedBox(
+                  width: 350,
+                  child: Text(
+                    'Send Email',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'Arial Narrow',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40.0),
               const Text(
                 'Enter Verification Code:',
                 style: TextStyle(
@@ -85,41 +163,24 @@ class _VerificationPageState extends State<VerificationPage> {
               ),
               const SizedBox(height: 20.0),
               ElevatedButton(
-                onPressed: () => _handleEmail(context), // Pass context here
+                onPressed: () => _handleSendCode(context),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF14532d),
-                ),
-                child: const Text(
-                  'Submit',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                  backgroundColor: const Color(0xFF14532d).withBlue(255),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  padding: const EdgeInsets.symmetric(vertical: 13.0),
                 ),
-              ),
-              const SizedBox(height: 10.0), // Add some space between buttons
-              ElevatedButton(
-                onPressed: () async {
-                  final currentContext = context; // Store the context in a variable
-                  try {
-                    await sendCode();
-                  } catch (e) {
-                    if (!currentContext.mounted) return;
-                    ScaffoldMessenger.of(currentContext).showSnackBar(
-                      const SnackBar(
-                        content: Text('An error occurred. Please try again...'),
-                      ),
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF14532d),
-                ),
-                child: const Text(
-                  'Send Code',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                child: const SizedBox(
+                  width: 350,
+                  child: Text(
+                    'Submit Code',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'Arial Narrow',
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
