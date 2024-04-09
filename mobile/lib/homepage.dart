9cd3761f875b4052914ecc6dc228034c94cf7ee2
katerinagarkova <156254295+katerinagarkova@users.noreply.github.com>
2024-04-09 00:03:59 -0400

    Update homepage.dart

--- mobile/lib/homepage.dart
+++ mobile/lib/homepage.dart
@@ -1,128 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/main.dart';
-class HomePageScreen extends StatefulWidget {
-  @override
-  _HomePageScreenState createState() => _HomePageScreenState();
-}
-class _HomePageScreenState extends State<HomePageScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
+
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        width: 400,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  width: 200,
-                  child: Material( // Ensure TextField has a Material ancestor
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: 'Search',
-                        hintText: 'Search for a Card',
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10), // Add some spacing between TextField and ElevatedButton
-                ElevatedButton(
-                  onPressed: () {
-                    // Add functionality for the Search button
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.brown[50]), // Background color
-                    foregroundColor: MaterialStateProperty.all(Colors.black), // Text color
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12.0)), // Button padding
-                    overlayColor: MaterialStateProperty.all(Colors.grey[100]), // Splash color
-                  ),
-                  child: Text(
-                    'Search',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: 200,
-                  child: Material( // Ensure TextField has a Material ancestor
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: 'Add',
-                        hintText: 'Add a Card',
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10), // Add some spacing between TextField and ElevatedButton
-                ElevatedButton(
-                  onPressed: () {
-                    // Add functionality for the Add button
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.brown[50]), // Background color
-                    foregroundColor: MaterialStateProperty.all(Colors.black), // Text color
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12.0)), // Button padding
-                    overlayColor: MaterialStateProperty.all(Colors.grey[100]), // Splash color
-                  ),
-                  child: Text(
-                    'Add',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    MaterialPageRoute(builder: (context) => const LoginPage());
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.brown[50]), // Background color
-                    foregroundColor: MaterialStateProperty.all(Colors.black), // Text color
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12.0)), // Button padding
-                    overlayColor: MaterialStateProperty.all(Colors.grey[100]), // Splash color
-                  ),
-                  child: Text(
-                    'Logout',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
-class MainPage extends StatefulWidget {
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _MainPageState createState() => _MainPageState();
+  State<HomePage> createState() =>
+      _HomePageState();
 }
-class _MainPageState extends State<MainPage> {
-  @override
-  void initState() {
-    super.initState();
+
+class _HomePageState
+    extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Timer',
+      style: optionStyle,
+    ),
+    Text(
+      'Alarm',
+      style: optionStyle,
+    ),
+    Text(
+      'To-Do',
+      style: optionStyle,
+    ),
+    Text(
+      'Calendar',
+      style: optionStyle,
+    ),
+    Text(
+      'Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Locked In'),
+        centerTitle: true,
+        backgroundColor: Colors.amber[800],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Timer',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'Alarm',
+            backgroundColor: Color.fromARGB(255, 235, 204, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task),
+            label: 'To-Do',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.teal,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
