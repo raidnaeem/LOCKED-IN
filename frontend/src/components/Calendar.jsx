9a124236d835f3bba971aac4f9004eeebf2120eb
AndrewZ496 <an719055@ucf.edu>
2024-04-09 12:19:30 -0400

    Fixing Calendar

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
 
 const Calendar = () => {
-    return (
-        <div className="relative bg-stone-400 shadow border border-black w-[80%] md:w-[1440px] h-[80%] md:h-[1024px] mx-auto">
-            {/* Container for Month and Year */}
-            <div className="absolute left-[145px] top-[66px] md:left-[50%] md:transform md:-translate-x-1/2">
-                <div className="text-white text-5xl font-normal font-montagu-slab">MONTH</div>
-                <div className="text-white text-5xl font-normal font-montagu-slab mt-4">YEAR</div>
-            </div>
-
-            {/* Box 1 - Example: Rotated Box */}
-            <div className="absolute left-[80px] top-[562px]">
-                <div className="w-12 h-12 bg-zinc-300 bg-opacity-50 rounded-full transform rotate-180"></div>
-            </div>
-
-            {/* Box 2 - Example: Add Event */}
-            <div className="absolute left-[145px] top-[921px]">
-                <div className="flex items-center">
-                    <div className="w-12 h-12 bg-amber-300 rounded-full"></div>
-                    <div className="text-white text-3xl font-normal font-roboto ml-4">Add Event</div>
-                </div>
-            </div>
-
-            {/* Box 3 - Example: Search Event */}
-            <div className="absolute left-[1360px] top-[512px]">
-                <div className="w-12 h-12 bg-zinc-300 bg-opacity-50 rounded-full"></div>
-            </div>
-
-            {/* Box 4 - Example: Nested Box with Border */}
-            <div className="absolute left-[449px] top-[921px]">
-                <div className="w-12 h-12 bg-amber-300 rounded-full">
-                    <div className="w-8 h-8 bg-pink-50 border border-white rounded-full ml-2"></div>
-                </div>
-            </div>
-
-            {/* Search Event Text */}
-            <div className="absolute left-[521px] top-[913px] text-white text-3xl font-normal font-roboto">
-                Search Event
-            </div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-4/5 md:w-1/1 bg-stone-400 shadow border border-black p-8 text-center">
+        <div className="text-white text-5xl md:text-4xl font-normal font-montagu-slab">
+          MONTH
         </div>
-    );
+        <div className="text-white text-5xl md:text-4xl font-normal font-montagu-slab">
+          YEAR
+        </div>
+        <div className="mt-8">
+          <div className="w-12 h-12 bg-zinc-300 bg-opacity-50 rounded-full mx-auto"></div>
+        </div>
+        <div className="mt-8">
+          <div className="flex items-center justify-center">
+            <div className="w-12 h-12 bg-amber-300 rounded-full"></div>
+            <div className="text-white text-3xl md:text-2xl font-normal font-roboto ml-4">
+              Add Event
+            </div>
+          </div>
+        </div>
+        <div className="mt-8">
+          <div className="w-12 h-12 bg-zinc-300 bg-opacity-50 rounded-full mx-auto"></div>
+        </div>
+        <div className="mt-8">
+          <div className="flex items-center justify-center">
+            <div className="w-12 h-12 bg-amber-300 rounded-full"></div>
+            <div className="w-8 h-8 bg-pink-50 border border-white rounded-full ml-2"></div>
+            <div className="text-white text-3xl md:text-2xl font-normal font-roboto ml-4">
+              Search Event
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
--- frontend/src/pages/CalendarPage.js
+++ frontend/src/pages/CalendarPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PageTitle from '../components/PageTitle';
 import LoggedInName from '../components/LoggedInName';
 import Calendar from '../components/Calendar';
+import DayGridComponent from '../components/DayGridComponent';
 
 const CalendarPage = () => {
   return (
@@ -9,6 +10,7 @@ const CalendarPage = () => {
       <PageTitle />
       <LoggedInName />
       <Calendar />
+      <DayGridComponent />
     </div>
   );
 };
