3cb29cea237bf8e8f52906a0ccffe6794f5f11e7
AndrewZ496 <an719055@ucf.edu>
2024-04-09 11:39:40 -0400

    Adding Calendar

--- frontend/src/pages/CalendarPage.js
+++ frontend/src/pages/CalendarPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import LoggedInName from '../components/LoggedInName';
-import Janurary from '../components/Janurary';
+import Calendar from '../components/Calendar';
 
 const CalendarPage = () => {
   return (
     <div>
       <PageTitle />
       <LoggedInName />
-      <Janurary />
+      <Calendar />
     </div>
   );
 };
