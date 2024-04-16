727da582f9743a3e5197bbc3321dce471d48b6cf
AndrewZ496 <an719055@ucf.edu>
2024-04-16 15:20:30 -0400

    Register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -34,17 +34,19 @@ function Register() {
 
         // Check for uppercase letter
         if (!/[A-Z]/.test(registerPassword)) {
-            passwordErrors.push('Password must contain an uppercase letter\n');
+            passwordErrors.push('Password must contain a uppercase letter.\n');
         }
 
         // Check for special character
         if (!/[!@#$%^&*]/.test(registerPassword)) {
-            passwordErrors.push('Password must contain a special character (!@#$%^&*)\n');
+            passwordErrors.push('Password must contain a special character.\n');
         }
 
         // If there are multiple errors, combine them into a single message
         if (passwordErrors.length > 0) {
-            setMessage(passwordErrors.join(''));
+            setMessage('Password must be at least 10 characters long.\n');
+            setMessage('Password must contain a uppercase letter.\n');
+            setMessage('Password must contain a special character.\n');
             return;
         }
 
