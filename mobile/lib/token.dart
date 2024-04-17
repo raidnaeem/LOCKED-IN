7ebe1fb385223a07db0626cfaa29498148bfad12
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2024-04-17 00:19:36 -0400

    Update token.dart
    Added functionality to todo for add and search
    Need to finish edit and delete

--- mobile/lib/token.dart
+++ mobile/lib/token.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 
 dynamic token;
 dynamic verify;
 dynamic sent;
 dynamic validEmail;
-late String userId;
+dynamic userId;
 
 String emailVerify = '';
 String code = '';
@@ -15,7 +15,7 @@ String passwordResetToken = '';
 Future<void> signUp(
     String firstName, String lastName, String email, String password) async {
   final Uri url =
-  Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/register');
+      Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/register');
   final Map<String, String> body = {
     'Email': email,
     'Password': password,
@@ -38,24 +38,17 @@ Future<void> signUp(
       final jsonResponse = json.decode(response.body);
       verify = jsonResponse['verified']; // Extracting the token string
       code = jsonResponse['AccessToken'];
-    }
-
-    else if (response.statusCode == 400) {
+    } else if (response.statusCode == 400) {
       throw 'User Already Exists With The Provided Email.';
-    }
-
-    else {
+    } else {
       throw 'Register Failed';
     }
-
-  }
-
-  catch (e) {
-  }
+  } catch (e) {}
 }
 
 Future<void> login(String email, String password) async {
-  final Uri url = Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/login');
+  final Uri url =
+      Uri.parse('https://locked-in-561ee2a901c9.herokuapp.com/api/login');
   final Map<String, String> body = {
     'Email': email,
     'Password': password,
@@ -72,25 +65,22 @@ Future<void> login(String email, String password) async {
       body: json.encode(body),
     );
 
-    print(response.statusCode);
-
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
+      userId = jsonResponse['UserID'];
       verify = jsonResponse['verified']; // Extracting the token string
       code = jsonResponse['AccessToken'];
-    }
-
-    else if (response.statusCode == 400) {
+    } else if (response.statusCode == 400) {
       throw 'Invalid Username or Password, Please Try Again';
     }
-
   } catch (e) {
     throw 'Invalid Username or Password, Please Try Again';
   }
 }
 
 Future<void> requestPasswordReset(String email) async {
-  const String apiUrl = 'https://locked-in-561ee2a901c9.herokuapp.com/api/request-password-reset';
+  const String apiUrl =
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/request-password-reset';
   final Map<String, String> requestBody = {'Email': email};
 
   try {
@@ -99,8 +89,6 @@ Future<void> requestPasswordReset(String email) async {
       headers: <String, String>{'Content-Type': 'application/json'},
       body: jsonEncode(requestBody),
     );
-
-    print(response.body);
 
     validEmail = response.statusCode;
 
@@ -116,11 +104,14 @@ Future<void> requestPasswordReset(String email) async {
   }
 }
 
-
-Future<void> sendPasswordResetEmail(String email, String passwordResetToken) async {
-  const String apiUrl = 'https://locked-in-561ee2a901c9.herokuapp.com/api/reset-password/:passwordResetToken';
-  final Map<String, String> requestBody =
-  {'Email': email, 'passwordResetToken': passwordResetToken};
+Future<void> sendPasswordResetEmail(
+    String email, String passwordResetToken) async {
+  const String apiUrl =
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/reset-password/:passwordResetToken';
+  final Map<String, String> requestBody = {
+    'Email': email,
+    'passwordResetToken': passwordResetToken
+  };
 
   try {
     final response = await http.post(
@@ -140,7 +131,8 @@ Future<void> sendPasswordResetEmail(String email, String passwordResetToken) asy
 }
 
 Future<void> verifyUser(String email, String verificationToken) async {
-  final String verificationUrl = 'https://locked-in-561ee2a901c9.herokuapp.com/verify-email/$verificationToken';
+  final String verificationUrl =
+      'https://locked-in-561ee2a901c9.herokuapp.com/verify-email/$verificationToken';
 
   try {
     final response = await http.post(
@@ -148,7 +140,60 @@ Future<void> verifyUser(String email, String verificationToken) async {
     );
 
     throw 'Verification Email Sent Successfully.\nPlease Check Your Email.';
+  } catch (error) {
+    throw '$error';
+  }
+}
 
+Future<void> addTask(
+    String task, String taskImage, bool done, int userId) async {
+  const String apiUrl =
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/task/add';
+
+  print("Task: $task");
+  final Map<String, dynamic> requestBody = {
+    'Task': task,
+    'TaskImage': null, // Ensure this object is structured correctly
+    'Done': done,
+    'UserID': userId
+  };
+
+  try {
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{'Content-Type': 'application/json'},
+      body: jsonEncode(requestBody),
+    );
+
+    //print(response.body);
+
+    if (response.statusCode == 200) {
+      throw 'Task added successfully';
+    } else {
+      throw 'Failed to add task. Status Code: ${response.statusCode}';
+    }
+  } catch (error) {
+    throw '$error';
+  }
+}
+
+Future<List<dynamic>> searchTasks(String query, int userID) async {
+  int page = 1;
+  const String apiUrl =
+      'https://locked-in-561ee2a901c9.herokuapp.com/api/todo/search';
+
+  final Map<String, dynamic> queryParams = {
+    'query': query,
+    'userID': userID.toString(),
+    'page': page.toString(),
+    'pageSize': '5',
+  };
+
+  final Uri uri = Uri.parse(apiUrl).replace(queryParameters: queryParams);
+
+  try {
+    final response = await http.get(uri);
+    return jsonDecode(response.body);
   } catch (error) {
     throw '$error';
   }
